299da95029787083d05505b1a4fbc0f0225907bd
Patrick Hansen <patrickhansen@college.harvard.edu>
2017-05-10 16:31:10 -0400

    fixed test

--- sweep_victqueuesize.sh
+++ sweep_victqueuesize.sh
@@ -30,7 +30,7 @@ g++ -Wall --std=c++11 -o pp2_512_8_8_8_0 percepts/pp2_512_8_8_8_0.cpp lib/config
 ./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
 ./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
 
-g++ -Wall --std=c++11 -o pp2_512_8_8_8_0 percepts/pp2_512_8_8_16_0.cpp lib/config1.a
+g++ -Wall --std=c++11 -o pp2_512_8_8_16_0 percepts/pp2_512_8_8_16_0.cpp lib/config1.a
 
 ./pp2_512_8_8_16_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
 ./pp2_512_8_8_16_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
