f2d4ac1feae169d2221f3133b2245c74a286879b
Patrick Hansen <patrickhansen@college.harvard.edu>
2017-05-10 16:27:54 -0400

    added tests

---
+++ sweep_tblsize_pushzerosON.sh
@@ -0,0 +1,41 @@
+## naming convention is pp2_(tblsize)_(str)_(thresh)_(victqueuesize)_(push zeroes)
+
+g++ -Wall --std=c++11 -o pp2_128_8_8_8_1 percepts/pp2_128_8_8_8_1.cpp lib/config1.a
+
+./pp2_128_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_128_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_128_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_128_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_128_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_256_8_8_8_1 percepts/pp2_256_8_8_8_1.cpp lib/config1.a
+
+./pp2_256_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_256_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_256_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_256_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_256_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_512_8_8_8_1 percepts/pp2_512_8_8_8_1.cpp lib/config1.a
+
+./pp2_512_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_1024_8_8_8_1 percepts/pp2_1024_8_8_8_1.cpp lib/config1.a
+
+./pp2_1024_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_1024_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_1024_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_1024_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_1024_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_2048_8_8_8_1 percepts/pp2_2048_8_8_8_1.cpp lib/config1.a
+
+./pp2_2048_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_2048_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_2048_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_2048_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_2048_8_8_8_1 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
---
+++ sweep_thres.sh
@@ -0,0 +1,41 @@
+## naming convention is pp2_(tblsize)_(str)_(thresh)_(victqueuesize)_(push zeroes)
+
+g++ -Wall --std=c++11 -o pp2_512_8_0_8_0 percepts/pp2_512_8_0_8_0.cpp lib/config1.a
+
+./pp2_512_8_0_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_0_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_0_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_0_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_0_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_512_8_1_8_0 percepts/pp2_512_8_1_8_0.cpp lib/config1.a
+
+./pp2_512_8_1_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_1_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_1_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_1_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_1_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_512_8_2_8_0 percepts/pp2_512_8_2_8_0.cpp lib/config1.a
+
+./pp2_512_8_2_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_2_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_2_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_2_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_2_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_512_8_4_8_0 percepts/pp2_512_8_4_8_0.cpp lib/config1.a
+
+./pp2_512_8_4_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_4_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_4_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_4_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_4_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
+
+g++ -Wall --std=c++11 -o pp2_512_8_8_8_0 percepts/pp2_512_8_8_8_0.cpp lib/config1.a
+
+./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/graph_analytics_10M.trace.gz
+./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/libquantum_10M.trace.gz
+./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/mcf_10M.trace.gz
+./pp2_512_8_8_8_0 -warmup_instructions 2000000 -simulation_instructions 10000000 -traces trace/xalancbmk_10M.trace.gz
