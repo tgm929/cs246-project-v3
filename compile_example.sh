0bb6966e3a8990c82c9c54b1742bbebf280df296
tgm929 <tmcnamara@college.harvard.edu>
2017-05-10 16:05:15 -0400

    made first compile and run file for sweeps

--- compile_example.sh
+++
@@ -1,3 +0,0 @@
-g++ -Wall --std=c++11 -o percep-c1 example/percept_predict.cpp lib/config1.a
-
-./percep-c1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
