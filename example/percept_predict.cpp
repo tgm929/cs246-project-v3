e0af98b377869509e29894b8f8b32d7142bcd533
Patrick Hansen <patrickhansen@college.harvard.edu>
2017-05-09 20:35:49 -0400

    added new feature tables and hash functions '

--- example/percept_predict.cpp
+++ example/percept_predict.cpp
@@ -7,17 +7,23 @@
 #define MAX_CTR 8
 #define THRESH 4
 #define VICTIM_QUEUE_SIZE LLC_WAYS/4
+#define PCi_SIZE 3
 
 uint32_t lru[LLC_SETS][LLC_WAYS];
 bool zeroReuse[LLC_SETS][LLC_WAYS];
 int32_t predictions[LLC_SETS][LLC_WAYS];
 int feat1[TBLSIZE];
+int feat2[TBLSIZE];
+int feat3[TBLSIZE];
+int PCi[PCi_SIZE];
 
 struct victqueue_entry
 {
-    int tag;
+    uint32_t tag;
     int pos;
-    int hash1;
+    uint32_t hash1;
+    uint32_t hash2;
+    uint32_t hash3;
 };
 
 victqueue_entry vict_queue[LLC_SETS][VICTIM_QUEUE_SIZE];
@@ -38,12 +44,20 @@ void InitReplacementState()
             vict_queue[i][j].tag = 0;
             vict_queue[i][j].pos = 0;
             vict_queue[i][j].hash1 = 0;
+            vict_queue[i][j].hash2 = 0;
+            vict_queue[i][j].hash3 = 0;
         }
 
     }
 
     for (int i=0; i < TBLSIZE; i++) {
-    	feat1[TBLSIZE] = 0 ;
+    	feat1[TBLSIZE] = 0;
+        feat2[TBLSIZE] = 0;
+        feat3[TBLSIZE] = 0;
+    }
+
+    for (int i=0; i < PCi_SIZE; i++) {
+        PCi[i] = 0;
     }
 }
 
@@ -57,22 +71,49 @@ void InitReplacementState()
 
 }*/
 
-void updateTables(bool inc, int tableInd) {
-        if (inc)
-            feat1[tableInd]++;
-        else
-            feat1[tableInd]--;
+// updates PCi
+void updatePCi(uint64_t PC) {
+    for (int i = 0; i < PCi_SIZE - 1; i++) {
+        PCi[i + 1] = PCi[i];
+    }
+    PCi[0] = PC;
 }
 
-int getInsertPos(int tableInd) {
-	if (feat1[tableInd] < (0-THRESH))
+// returns the XOR of all PCi
+uint64_t getXOR_PCi() {
+    uint64_t result = 0;
+    for (int i = 0; i < PCi_SIZE; i++) {
+        result =  result ^ PCi[i];
+    }
+    return result;
+}
+
+// train
+void updateTables(bool inc, int index1, int index2, int index3) {
+        if (inc) {
+            feat1[index1]++;
+            feat2[index2]++;
+            feat3[index3]++;
+        }
+        else {
+            feat1[index1]--;
+            feat2[index2]--;
+            feat3[index3]--;
+        }
+}
+
+// predict
+int getInsertPos(int index1, int index2, int index3) {
+    int sum = feat1[index1] + feat2[index2] + feat3[index3];
+	if (sum < (0-THRESH))
 		return -1;
-	else if (feat1[tableInd > THRESH])
+	else if (sum > THRESH)
 		return 1;
 	else 
 		return 0;
 }
 
+// check match in vicitim queue
 bool checkVictQueue (uint32_t set, uint32_t tag) {
     for (int i = 0; i < VICTIM_QUEUE_SIZE; i++) {
         if (vict_queue[set][i].tag == tag)
@@ -81,7 +122,8 @@ bool checkVictQueue (uint32_t set, uint32_t tag) {
     return false;
 }
 
-void updateVictQueue (uint32_t set, uint32_t tag, int hv1) {
+// insert victim into queue and push other entries down 1
+void updateVictQueue (uint32_t set, uint32_t tag, int hv1, int hv2, int hv3) {
     for (int i = 0; i < VICTIM_QUEUE_SIZE; i++) {
         if (vict_queue[set][i].pos < (VICTIM_QUEUE_SIZE - 1)) {
             vict_queue[set][i].pos++;
@@ -90,6 +132,8 @@ void updateVictQueue (uint32_t set, uint32_t tag, int hv1) {
             vict_queue[set][i].pos = 0;
             vict_queue[set][i].tag = tag;
             vict_queue[set][i].hash1 = hv1;
+            vict_queue[set][i].hash2 = hv2;
+            vict_queue[set][i].hash3 = hv3;
         }
     }
 }
@@ -97,27 +141,37 @@ void updateVictQueue (uint32_t set, uint32_t tag, int hv1) {
 
 // find replacement victim
 // return value should be 0 ~ 15 or 16 (bypass)
+// training called here
 uint32_t GetVictimInSet (uint32_t cpu, uint32_t set, const BLOCK *current_set, uint64_t PC, uint64_t paddr, uint32_t type)
 {
+    // find victim
     uint32_t lruInd = 0;
     for (int i=0; i<LLC_WAYS; i++)
         if (lru[set][i] == (LLC_WAYS-1))
             lruInd = i;
     
-    uint32_t hashVal = (paddr >> 6) % TBLSIZE;
+    // hash functions
+    uint32_t hv1 = (paddr >> 6) % TBLSIZE;
+    uint32_t hv2 = (PC >> 2) % TBLSIZE;
+    uint32_t hv3 = (getXOR_PCi() >> 2) % TBLSIZE;
+
+    // training and victim queue management
+    // if not reused
     if (zeroReuse[set][lruInd]) {
         if (predictions[set][lruInd] == 1) {
-            updateTables(false, hv1);
-            updateVictQueue (0, 0, 0);
+            updateTables(false, hv1, hv2, hv3);
+            // updateVictQueue (0, 0, 0, 0, 0);
+        }
+        else if (predictions[set][lruInd] == 0) {
+            updateTables(false, hv1, hv2, hv3);
         }
         else {
-            updateVictQueue(set, BLOCK->tag, hv1);
+            updateVictQueue(set, current_set->tag, hv1, hv2, hv3);
         }
     }
-    else {
-        if (predictions[set][lruInd] == -1) {
-            updateTables(true, hv1);
-        }
+    // if reused and predicted 0 or -1
+    else if (predictions[set][lruInd] == 0 || predictions[set][lruInd] == -1) {
+            updateTables(true, hv1, hv2, hv3);
     }
 
     return lruInd;
@@ -126,6 +180,9 @@ uint32_t GetVictimInSet (uint32_t cpu, uint32_t set, const BLOCK *current_set, u
 // called on every cache hit and cache fill
 void UpdateReplacementState (uint32_t cpu, uint32_t set, uint32_t way, uint64_t paddr, uint64_t PC, uint64_t victim_addr, uint32_t type, uint8_t hit)
 {
+    // update PCi
+    updatePCi(PC);
+
     // update lru replacement state
     // if its a hit, update the lru as normal
     if (hit) {
@@ -142,14 +199,16 @@ void UpdateReplacementState (uint32_t cpu, uint32_t set, uint32_t way, uint64_t 
 	}
         // if new insertion, choose where to insert the new block
     else {
-        uint32_t hashVal = (paddr >> 6) % TBLSIZE;
-        int prediction = getInsertPos(hashVal);
+        uint32_t hv1 = (paddr >> 6) % TBLSIZE;
+        uint32_t hv2 = (PC >> 2) % TBLSIZE;
+        uint32_t hv3 = (getXOR_PCi() >> 2) % TBLSIZE;
+        int prediction = getInsertPos(hv1, hv2, hv3);
         predictions[set][way] = prediction;
         uint32_t insertPos = 0;
         if (prediction == -1)
         	insertPos = LLC_WAYS - 1;
         else if (prediction == 0)
-        	insertPos = LLC_WAYS / 2;
+        	insertPos = (LLC_WAYS / 2) - 1;
         else
         	insertPos = 0;
         for (uint32_t i=0; i < LLC_WAYS; i++) {
